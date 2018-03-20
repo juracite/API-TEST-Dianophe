c63dcc18505c6866e6ce2f73ac7da055b34e6bad
YLM <yannlm2010@hotmail.fr>
2018-03-20 21:28:30 +0100

    Add route [user] get user information

--- app/routesTest/users.js
+++ app/routesTest/users.js
@@ -9,6 +9,52 @@ function initUsers(app) {
     res.setHeader("Content-Type", "application/json");
     return res.send(JSON.stringify(data, null, 4));
   });
+  app.get("/test/user/:id", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+
+    let reg = /^\d+$/;
+    let id = req.params.id;
+
+    if (reg.test(id)) {
+      data.forEach(user => {
+        if (user.id == id) {
+          return res.send(
+            JSON.stringify(
+              {
+                id: id,
+                username: user.username,
+                nbr_tickets: user.nbr_tickets,
+                account_type: user.account_type,
+                img_url: user.img_url
+              },
+              null,
+              4
+            )
+          );
+        }
+      });
+    } else {
+      return res.send(
+        JSON.stringify(
+          {
+            error: "Nombre/Chiffres uniquement !"
+          },
+          null,
+          4
+        )
+      );
+    }
+
+    return res.send(
+      JSON.stringify(
+        {
+          error: "Utilisateur introuvable."
+        },
+        null,
+        4
+      )
+    );
+  });
 }
 
 module.exports = initUsers;
